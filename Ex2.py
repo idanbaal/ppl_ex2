d6ee015fd669a60c7c75d652543af4bee27a96ff
idanbaal <idanbaal@post.bgu.ac.il>
2018-05-07 17:31:41 +0300

    Question 3 Changes

--- Ex2.py
+++ Ex2.py
@@ -1,5 +1,3 @@
-
-
 class Person:
 
     def __init__(self, first, last):
@@ -13,26 +11,26 @@ class Person:
 class Employee(Person):
 
     def __init__(self, first, last, staffnum):
-        Person.__init__(self,first, last)
+        Person.__init__(self, first, last)
         self.staffnumber = staffnum
 
     def GetEmployee(self):
-        return self.Name() + ", " +  self.staffnumber
+        return self.Name() + ", " + self.staffnumber
 
 
 class Engineer(Employee):
 
-    def __init__(self, first, last, staffnum,profession ):
-        Employee.__init__(self,first, last,staffnum)
+    def __init__(self, first, last, staffnum, profession):
+        Employee.__init__(self, first, last, staffnum)
         self.profession = profession
 
-
     def GetEngineer(self):
-        return self.Name() + ", " +  self.profession
+        return self.Name() + ", " + self.profession
+
 
 class Cat():
 
-    def __init__(self, name ):
+    def __init__(self, name):
         self.name = name
 
 
@@ -40,9 +38,7 @@ c = Cat("Mizi")
 
 x = Person("Marge", "Simpson")
 y = Employee("Homer", "Simpson", "1007")
-z = Engineer("Homer", "Simpson", "1007","Software")
-
-
+z = Engineer("Homer", "Simpson", "1007", "Software")
 
 
 ###Question 2###
@@ -50,20 +46,24 @@ z = Engineer("Homer", "Simpson", "1007","Software")
 def isClass(classInfo):
     return str(type(classInfo)) == "<type 'classobj'>" or str(type(classInfo)) == "<type 'type'>"
 
+
 def isInstance(object1):
-    return str(type(object1))== "<type 'instance'>"
+    return str(type(object1)) == "<type 'instance'>"
+
 
 def isInstancePPL(object1, classInfo):
     if isInstance(object1) and isClass(classInfo):
         return object1.__class__ is classInfo
+    return False
 
 
 def numInstancePPL(object1, classInfo):
     if isInstance(object1) and isClass(classInfo):
-        if isInstancePPL(object1,classInfo):
+        if isInstancePPL(object1, classInfo):
             return 1
         else:
-            return numSubclassPPL(object1.__class__,classInfo)
+            return numSubclassPPL(object1.__class__, classInfo)
+    return 0
 
 
 def isSubclassPPL(class1, classInfo):
@@ -71,37 +71,57 @@ def isSubclassPPL(class1, classInfo):
         if class1 == classInfo:
             return True
         elif class1.__bases__ != ():
-            return isSubclassPPL(class1.__bases__[0],classInfo)
-        return False
+            return isSubclassPPL(class1.__bases__[0], classInfo)
+    return False
 
 
 def numSubclassPPL(class1, classInfo):
     if isClass(class1) and isClass(classInfo):
         if class1 == classInfo:
             return 1
-        elif class1.__bases__ != ():
-            return 1 + numSubclassPPL(class1.__bases__[0],classInfo)
-        return 0
-
-print type(y)
+        elif class1.__bases__ != () and isSubclassPPL(class1, classInfo):
+            return 1 + numSubclassPPL(class1.__bases__[0], classInfo)
+    return 0
 
 
+# print type(y)
 
 ###Question 3###
 
-def count_if(lst ,func):
-    return len(filter(func ,lst))
+def count_if(lst, func):
+    if type(lst) == list and callable(func):
+        try:
+            return len(filter(func, lst))
+        except:
+            return 0
+    return 0
 
 
-def for_all(lst ,apply ,pred):
-    return all(map(pred ,map(apply ,lst)))
+def for_all(lst, apply, pred):
+    if type(lst) == list and callable(apply) and callable(pred):
+        try:
+            return all(map(pred, map(apply, lst)))
+        except:
+            return False
+    return False
 
 
-def for_all_red(lst ,apply ,pred):
-    return pred(reduce(apply, lst))
+def for_all_red(lst, apply, pred):
+    if type(lst) == list and callable(apply) and callable(pred):
+        try:
+            return pred(reduce(apply, lst))
+        except:
+            return False
+    return False
 
 
-def there_exists(lst,n,pred):
-    return sum(map(pred,lst)) >= n
+def there_exists(lst, n, pred):
+    if type(lst) and type(n) == int and callable(pred):
+        try:
+            return sum(map(pred, lst)) >= n
+        except:
+            return False
+    return False
+
 
 
